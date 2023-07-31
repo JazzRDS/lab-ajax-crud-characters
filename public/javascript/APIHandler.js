e8e07cab29350ef18da7ab0209f7915d9e2027d5
JazzRDS <jazzrds@gmail.com>
2023-07-31 08:31:50 +0100

    ummm yeah

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,68 @@
+import axios from "axios"
+
 class APIHandler {
   constructor (baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
-
+  async getFullList()  {
+    const response = await axios.get("http://localhost:8000/characters");
+    console.log (response)
+    if (response.status === 200){
+      
+     return response.data;
+    }
   }
 
-  getOneRegister () {
+ async getOneRegister (id) {
+  const response = await axios.get("http://localhost:8000/characters/:id");
+  console.log (response)
+  if (response.status === 200){
 
+    return response.data;
+  }
   }
 
-  createOneRegister () {
-
+  async createOneRegister (characterInfo) {
+    try {const response = await axios
+      .post("http://localhost:8000/characters/", characterInfo);
+    
+      return response.data;
+    }
+    catch (error){
   }
+}
+  
 
-  updateOneRegister () {
-
+async updateOneRegister(id, characterInfo){
+  try {
+    const response = await axios.put("http://localhost:8000/characters/:id", characterInfo);
+    if (response.status === 200) {
+      return response.data;
+    } else {
+      console.error('Error updating character:', response.statusText);
+      throw new Error('Failed to update character.');
+    }
+  } catch (error) {
+    console.error('Error updating character:', error);
+    throw error;
   }
+}
 
-  deleteOneRegister () {
 
+
+async deleteOneRegister(id) {
+  try {
+    const response = await axios.delete("http://localhost:8000/characters/:id");
+    if (response.status === 200) {
+      return response.data;
+    } else {
+      console.error('Error deleting character:', response.statusText);
+      throw new Error('Failed to delete character.');
+    }
+  } catch (error) {
+    console.error('Error deleting character:', error);
+    throw error;
   }
+}
 }
